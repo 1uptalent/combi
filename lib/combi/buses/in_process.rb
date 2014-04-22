b79d0b27c7368d2560402a852d3fd804e45e43d7
Abel Muiño <amuino@gmail.com>
2014-04-22 11:08:27 +0200

    Prototype timeouts with in_process services

--- lib/combi/buses/in_process.rb
+++ lib/combi/buses/in_process.rb
@@ -3,15 +3,18 @@ require 'combi/buses/bus'
 module Combi
   class InProcess < Bus
 
-    def request(handler, kind, message, options = {timeout: 0.1}, &block)
+    def request(handler, kind, message, options = {}, &block)
+      options[:timeout] ||= RPC_DEFAULT_TIMEOUT
       handler = memory_handlers[handler.to_s]
       return if handler.nil?
       service_instance = handler[:service_instance]
       message = JSON.parse(message) if message.is_a?(String)
       return unless service_instance.respond_to?(kind)
-      response = service_instance.send(kind, message)
-      response = response.call if response.is_a?(Proc)
-      block.call response
+      Timeout.timeout(options[:timeout]) do
+        response = service_instance.send(kind, message)
+        response = response.call if response.is_a?(Proc)
+        block.call response
+      end
     end
 
     def respond_to(service_instance, handler, options = {})
--- spec/lib/combi/buses/in_process_spec.rb
+++ spec/lib/combi/buses/in_process_spec.rb
@@ -26,7 +26,29 @@ describe 'Combi::InProcess' do
     When(:service) { subject.add_service boring_salutation_service }
     When(:params) { { who: 'world' } }
     Then do 
-      subject.request(:say_hello, :do_it, params.to_json) {|x| x} == "Hello world"
+      subject.request(:say_hello, :do_it, params.to_json) do |result|
+        result == "Hello world"
+      end
+    end
+  end
+
+  context 'raises Timeout::Error when the response is slow' do
+    Given(:slow_service) do
+      Module.new do
+        def actions; [:sleep]; end
+        def do_it(params)
+          sleep params['time']
+        end
+      end
+    end
+    When(:params) { { time: 0.001 } }
+    When(:service) { subject.add_service slow_service }
+    Then do
+      expect do
+        subject.request(:sleep, :do_it, params.to_json, { timeout: params[:time]/2.0 }) do
+          raise "Should never get here"
+        end
+      end.to raise_error Timeout::Error
     end
   end
 end
