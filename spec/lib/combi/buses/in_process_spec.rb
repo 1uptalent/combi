6f5f032739f86cf8351671caf75e47f0bcef3e99
Abel Muiño <amuino@gmail.com>
2014-04-22 11:53:52 +0200

    Move in_process bus tests to shared examples

--- spec/lib/combi/buses/in_process_spec.rb
+++ spec/lib/combi/buses/in_process_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'shared_examples/standard_bus'
 
 require 'combi/service_bus'
 require 'combi/buses/in_process'
@@ -10,45 +11,6 @@ describe 'Combi::InProcess' do
   end
 
   Given(:subject) { Combi::ServiceBus.init_for(:in_process, {}) }
-  Given(:boring_salutation_service) do
-    Module.new do
-      def actions
-        [:say_hello]
-      end
 
-      def do_it(params)
-        "Hello #{params['who']}"
-      end
-    end
-  end
-
-  context 'can invoke services' do
-    When(:service) { subject.add_service boring_salutation_service }
-    When(:params) { { who: 'world' } }
-    Then do 
-      subject.request(:say_hello, :do_it, params.to_json) do |result|
-        result == "Hello world"
-      end
-    end
-  end
-
-  context 'raises Timeout::Error when the response is slow' do
-    Given(:slow_service) do
-      Module.new do
-        def actions; [:sleep]; end
-        def do_it(params)
-          sleep params['time']
-        end
-      end
-    end
-    When(:params) { { time: 0.001 } }
-    When(:service) { subject.add_service slow_service }
-    Then do
-      expect do
-        subject.request(:sleep, :do_it, params.to_json, { timeout: params[:time]/2.0 }) do
-          raise "Should never get here"
-        end
-      end.to raise_error Timeout::Error
-    end
-  end
+  it_behaves_like 'standard_bus'
 end
