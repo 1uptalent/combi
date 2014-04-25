23f42320fdb2396ad125f5ad0ffbbae50130632d
German DZ <germ@ndz.com.ar>
2014-04-25 20:58:37 +0200

    ensure reactor is stopped when stop is required

--- spec/support/event_machine.rb
+++ spec/support/event_machine.rb
@@ -1,4 +1,5 @@
 require 'eventmachine'
+
 def start_background_reactor
   EM::error_handler do |error|
     STDERR << "ERROR IN EM\n"
@@ -6,13 +7,6 @@ def start_background_reactor
     STDERR << error.backtrace << "\n"
   end
   puts "-EM.start- the reactor is running: #{EM::reactor_running?}"
-  if EM::reactor_running?
-    puts "\tWaiting for the reactor to stop"
-    10.times do
-      sleep 0.1 if EM::reactor_running?
-      EM::stop_event_loop if EM::reactor_running?
-    end
-  end
   raise "EM did not shut down" if EM::reactor_running?
   Thread.new do
     puts "------- starting EM reactor"
@@ -33,4 +27,7 @@ end
 def stop_background_reactor
   puts "-EM.stop- the reactor is running: #{EM::reactor_running?}"
   EM::stop_event_loop if EM::reactor_running?
+  50.times do
+    sleep 0.3 if EM::reactor_running?
+  end
 end
