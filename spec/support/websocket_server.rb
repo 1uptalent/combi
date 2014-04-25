9b98997924499692055a02d6c35ddb83da0c5bb0
German DZ <germ@ndz.com.ar>
2014-04-25 20:59:21 +0200

    cleanup

--- spec/support/websocket_server.rb
+++ spec/support/websocket_server.rb
@@ -1,32 +1,7 @@
 require 'em-websocket'
 
-def start_websocket_server(websocket_bus)
-  ws_handler = Class.new do
-    def new_session(arg); end
-  end.new
-
-  pid = fork do
-    EM.error_handler do |error|
-      puts "\tERROR"
-      puts "\t#{error.inspect}"
-      puts error.backtrace
-    end
-    EM::WebSocket.start(host: '0.0.0.0', port: 9292) do |ws|
-      websocket_bus.manage_ws_event(ws, ws_handler)
-    end
-    puts "Websocket server stopped"
-  end
-  puts "Started websocket server with pid: #{pid}"
-  pid
-end
-
-def stop_websocket_server(pid)
-  puts "Stopping websocket server with pid: #{pid}"
-  Process.kill "KILL", pid
-end
-
 def start_em_websocket_server(websocket_bus, port)
-  puts "starting webserver with #{websocket_bus.inspect}"
+  puts "starting webserver at port #{port} with #{websocket_bus.inspect}"
   ws_handler = Class.new do
     def new_session(arg); end
   end.new
