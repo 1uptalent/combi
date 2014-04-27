56c590095430a77005f70738312223ed8f35263a
German DZ <germ@ndz.com.ar>
2014-04-27 12:10:16 +0200

    web socket server helper must run in reactor

--- spec/support/websocket_server.rb
+++ spec/support/websocket_server.rb
@@ -1,12 +1,14 @@
 require 'em-websocket'
 
 def start_em_websocket_server(websocket_bus, port)
-  puts "starting webserver at port #{port} with #{websocket_bus.inspect}"
+  puts "starting websocket server at port #{port}"
   ws_handler = Class.new do
     def new_session(arg); end
   end.new
-  EM::WebSocket.run(host: '0.0.0.0', port: port) do |ws|
-    websocket_bus.manage_ws_event(ws, ws_handler)
+  EM::next_tick do
+    EM::WebSocket.start(host: '0.0.0.0', port: port) do |ws|
+      websocket_bus.manage_ws_event(ws, ws_handler)
+    end
   end
   puts "EM WEBSOCKET STARTED"
   sleep 1
