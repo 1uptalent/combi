12fe29b6afa6e6824bfbbfcd4020fe9dedfabd26
German DZ <germ@ndz.com.ar>
2014-05-30 09:00:41 +0200

    remove dead code

--- lib/combi/helpers.rb
+++ lib/combi/helpers.rb
@@ -4,10 +4,8 @@ module Combi
 
   def self.wait_for(defer, options = {}, &block)
     options[:timeout] ||= 2
-    resolved = false
     waiter_thread = Thread.current
     defer.callback { |response|
-      resolved = true
       block.call response
       waiter_thread.wakeup
     }
